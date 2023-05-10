944ed3b25e9ca38f192a3bee3ebef5d188569163
manoj.katam <manoj.v@marketsimplified.com>
2023-05-10 15:07:13 +0530

    viewpager and tablayout

--- app/src/main/java/com/example/mytablayout/ChatsFragment.java
+++ app/src/main/java/com/example/mytablayout/ChatsFragment.java
@@ -17,6 +17,6 @@ public class ChatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         //
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        return inflater.inflate(R.layout.fragment_chats, container, false);
     }
 }
